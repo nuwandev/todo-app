771cd4fadb7147dbf20649370795aa6cbbf008de
Theekshana Nuwan <theekshanabro40@gmail.com>
2025-11-27 14:55:24 +0530

    Refactor TaskElement component to conditionally render completed tasks with strikethrough styling

--- src/components/Task.ts
+++ src/components/Task.ts
@@ -1,15 +1,30 @@
 import type { TTask } from "../types";
 
 const TaskElement = (task: TTask) => {
-  return `
+  return task.completed
+    ? `
     <li
-        class="flex items-center justify-between bg-gray-50 p-3 rounded-lg shadow-sm cursor-grab hover:bg-gray-100"
-      >
-        <span class="text-gray-700">${task.task}</span>
-        <button id="deleteTaskBtn" data-task-id="${task.id}" class="text-red-500 hover:text-red-700 cursor-pointer">
-          Delete
-        </button>
-      </li>
+      id="taskItem"
+      data-task-id="${task.id}"
+      class="flex items-center justify-between bg-green-50 p-3 rounded-lg shadow-sm cursor-grab hover:bg-green-100"
+    >
+      <span class="text-gray-700 line-through">${task.task}</span>
+      <button id="deleteTaskBtn" data-task-id="${task.id}" class="text-red-500 hover:text-red-700 cursor-pointer">
+        Delete
+      </button>
+    </li>
+      `
+    : `
+    <li
+      id="taskItem"
+      data-task-id="${task.id}"
+      class="flex items-center justify-between bg-gray-50 p-3 rounded-lg shadow-sm cursor-grab hover:bg-gray-100"
+    >
+      <span class="text-gray-700">${task.task}</span>
+      <button id="deleteTaskBtn" data-task-id="${task.id}" class="text-red-500 hover:text-red-700 cursor-pointer">
+        Delete
+      </button>
+    </li>
   `;
 };
 
